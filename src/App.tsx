2a0a985a8e5b3f155d5ce3017eac657ee490143c
konan-5 <ikedahiroshi517@gmail.com>
2024-07-25 14:32:21 +0000

    feat: Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import Router from './router';
+
 function App() {
   return (
-    <p className="text-2xl font-bold">
-      Click on the Vite and React logos to learn more
-    </p>
+    <Router />
   );
 }
 
