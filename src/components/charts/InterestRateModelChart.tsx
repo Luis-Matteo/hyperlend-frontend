aed9394d37720eb92652b815b44844372e5bb183
konan-5 <ikedahiroshi517@gmail.com>
2024-08-19 12:43:19 +0000

    feat: fix health factor and update token detail page

---
+++ src/components/charts/InterestRateModelChart.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+// Define the data type
+interface DataPoint {
+  year: string;
+  utilization?: number;
+  borrow?: number;
+  supply?: number;
+}
+
+// Sample data
+const data: DataPoint[] = [ 
+  { year: '2010', utilization: 10, borrow: 10, supply: 10 },
+  { year: '2011', utilization: 15, borrow: 12, supply: 11 },
+  { year: '2012', utilization: 20, borrow: 15, supply: 13 },
+  { year: '2013', utilization: 30, borrow: 20, supply: 17 },
+  { year: '2014', utilization: 40, borrow: 25, supply: 20 },
+  { year: '2015', utilization: 50, borrow: 35, supply: 25 },
+  { year: '2016', utilization: 60, borrow: 40, supply: 30 },
+  { year: '2017', utilization: 70, borrow: 50, supply: 35 },
+  { year: '2018', utilization: 80, borrow: 55, supply: 40 },
+  { year: '2019', utilization: 90, borrow: 60, supply: 50 },
+];
+
+const InterestRateModelChart: React.FC = () => {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart data={data} margin={{ top: 10, right: 30, left: 10, bottom: 10 }}>
+        <XAxis 
+          dataKey="year"  
+          axisLine={false} 
+          tickLine={false} 
+          padding={{ left: 30 }} // Add padding to create an empty space at the start
+          tick={{ angle: -45, dy: 15 }} // Rotate the X-axis labels
+        />
+        <YAxis 
+          axisLine={false} 
+          tickLine={false} 
+          tickFormatter={(value) => (value === 0 ? '' : value)} // Skip displaying the '0' value
+          domain={[10, 100]} // Set the Y-axis domain from 10 to 100
+          tickCount={10} // Ensure 10 ticks are displayed
+        />
+        <Tooltip />
+        <Line type="monotone" dataKey="utilization" stroke="#4c51bf" dot={false} name="Utilization rate" />
+        <Line type="monotone" dataKey="borrow" stroke="#38b2ac" dot={false} name="Borrow API" />
+        <Line type="monotone" dataKey="supply" stroke="#ecc94b" dot={false} name="Supply API" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default InterestRateModelChart;
---
+++ src/components/dashboard/Actions.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import ProgressBar from "../common/PercentBar";
+import CustomButton from "./CustomButton";
+import img from "../../assets/img";
+
+interface ActionProps {
+  amountTitle: string;
+  amountValue: string;
+  btnTitle: string;
+  inputDeal: string; // 80%, max ...
+  noteUnderInput?: JSX.Element;
+  noteAboveInput?: JSX.Element;
+}
+
+const Actions: React.FC<ActionProps> = ({
+  amountTitle,
+  amountValue,
+  inputDeal,
+  noteUnderInput,
+  noteAboveInput,
+  btnTitle
+}) => {
+  const [progress, setProgress] = useState<number>(80);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setProgress(Number(event.target.value));
+  };
+ 
+  return (
+    <div>
+      {noteAboveInput}
+      <div className="flex items-center justify-between bg-[#071311] rounded-md p-4 mt-4 mb-4">
+        <div className="flex gap-3 items-center w-fit p-3">
+            <img src={img.cat} alt="cat" />
+        <p className="text-base text-[#CAEAE566]">0.00</p>
+        </div>  
+        <div className="bg-[#081916] p-4 rounded">
+          <p className="text-base text-[#CAEAE566]">{inputDeal}</p>
+        </div>
+      </div>
+      {noteUnderInput}
+      <div className="mt-4">
+        <div className="flex justify-between items-center">
+          <p className="text-base text-lufga text-[#CAEAE5B2]">
+            {amountTitle} amount
+          </p>
+          <p className="text-base text-lufga text-[#CAEAE5]">{amountValue}</p>
+        </div>
+        <hr className="mt-4 mb-4 text-[#212325] border-t-[0.25px]" />
+      </div>
+      <div className="mt-4">
+        <div className="flex justify-between items-center">
+          <p className="text-base text-lufga text-[#CAEAE5B2]">Total APY</p>
+          <p className="text-base text-lufga text-[#CAEAE5]">9.92%</p>
+        </div>
+        <hr className="mt-4 mb-4 text-[#212325] border-t-[0.25px]" />
+      </div>
+      <div className="mt-4">
+        <div className="flex justify-between items-center">
+          <p className="text-base text-lufga text-[#CAEAE5B2]">Current: $918</p>
+          <p className="text-base text-lufga text-[#CAEAE5]">Max: $1505</p>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-between items-center mt-2 mb-2">
+          <p className="text-xs text-lufga text-[#CAEAE5B2]">Current: $918</p>
+          <p className="text-xs text-lufga text-[#CAEAE5]">Max: $1505</p>
+        </div>
+        <div className="flex justify-between items-center mt-2 mb-2">
+          <p className="text-xs text-lufga text-[#CAEAE5B2]">Borrow limit</p>
+          <p className="text-xs text-lufga text-[#CAEAE5]">$1505</p>
+        </div>
+        <div className="relative ">
+          <ProgressBar progress={progress} className="h-1.5" />
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={progress}
+            onChange={handleInputChange}
+            className="w-full top-0 left-0 absolute opacity-0 cursor-pointer"
+          />
+        </div>
+        <div className="flex justify-between items-center mt-2 mb-2">
+          <p className="text-xs text-lufga text-[#CAEAE5B2]">Daily earnings</p>
+          <p className="text-xs text-lufga text-[#2DC24E]">$687</p>
+        </div>
+      </div>
+      <CustomButton title={btnTitle} />
+    </div>
+  );
+};
+
+export default Actions;
