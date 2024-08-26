18059cf28c6dd2848c849b62caa2939ec4c6a3fe
fbslo <admin@fbslo.net>
2024-08-27 00:42:49 +0200

    remove search from navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'; 
+// import { useState } from 'react'; 
 import { useDispatch } from 'react-redux';
 import { toggleSidebar } from '../store/sidebarSlice';
-import magnifyIcon from '../assets/icons/magnify-icon.svg';
+// import magnifyIcon from '../assets/icons/magnify-icon.svg';
 import dashboardIcon from '../assets/icons/dashboard-icon.svg';
 
 type NavbarProps = {
@@ -12,7 +12,7 @@ type NavbarProps = {
 function Navbar({ pageTitle, pageIcon }: NavbarProps) {
   const dispatch = useDispatch();
   
-  const [searchText, setSearchText] = useState<string>('');
+  // const [searchText, setSearchText] = useState<string>('');
   return (
     <div>
       <div className="w-full flex justify-between items-center">
@@ -29,7 +29,7 @@ function Navbar({ pageTitle, pageIcon }: NavbarProps) {
           </h2>
         </div>
         <div className="flex items-center gap-4">
-          <div className="p-1 bg-primary hidden md:flex rounded-full gap-2">
+          {/* <div className="p-1 bg-primary hidden md:flex rounded-full gap-2">
             <div className="p-2 bg-gray-dark rounded-full">
               <img src={magnifyIcon} alt="" />
             </div>
@@ -38,7 +38,7 @@ function Navbar({ pageTitle, pageIcon }: NavbarProps) {
               placeholder="Search your coins..."
               onChange={(e) => { setSearchText(e.target.value); console.log(searchText); }}
             />
-          </div>
+          </div> */}
           <w3m-button />
         </div>
       </div>
