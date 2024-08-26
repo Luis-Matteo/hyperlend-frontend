1e614ca6f987de5a1931d91a9da0bad60d0c8903
konan-5 <ikedahiroshi517@gmail.com>
2024-08-26 07:53:21 +0200

    fix: card background

--- src/components/common/CardItem.tsx
+++ src/components/common/CardItem.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from 'react';
 
 type CardItemProps = {
-    className: string;
-    children: ReactNode
+  className: string;
+  children: ReactNode
 }
 
 function CardItem({ className, children }: CardItemProps) {
   return (
-    <div className={`bg-primary rounded-2xl ${className}`}>
+    <div className={`rounded-2xl ${className} ${className.includes('bg' ? '' : 'bg-primary')}`}>
       {children}
     </div>
   );
