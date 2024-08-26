0d696c15862200e3c70e905012b0c075ee1546b5
fbslo <admin@fbslo.net>
2024-08-26 23:13:06 +0200

    update toggle button to change color

--- src/components/common/ToggleButton.tsx
+++ src/components/common/ToggleButton.tsx
@@ -3,14 +3,17 @@ import React from "react";
 type ToggleButtonPropsProps = {
     status: boolean;
     setStatus: React.Dispatch<React.SetStateAction<boolean>>;
+    onClick?: any;
 };
 
-function ToggleButton({ status, setStatus }: ToggleButtonPropsProps) {
+function ToggleButton({ status, setStatus, onClick }: ToggleButtonPropsProps) {
     return (
         <button
             type="button"
-            className="p-0.5 bg-[#081916] rounded-full flex items-center"
-            onClick={() => { setStatus((prev) => !prev) }}
+            className={`p-0.5 rounded-full flex items-center duration-500 ${
+              status ? "bg-green-500" : "bg-[#081916]"
+            }`}
+            onClick={() => { setStatus((prev) => !prev); onClick ? onClick() : '' }}
         >
             <div
                 className={`p-2 rounded-full transition-all duration-500 ${status
