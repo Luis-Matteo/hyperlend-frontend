97804d65c1c97c9976e5b29918d6b370d5757cb6
konan-5 <ikedahiroshi517@gmail.com>
2024-10-25 17:08:00 +0200

    feat: Add share image download function

--- src/components/common/ShareImageModal.tsx
+++ src/components/common/ShareImageModal.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { useEffect } from 'react';
-import refreshIcon from '../../assets/icons/refresh.svg';
+import { toPng } from 'html-to-image';
+import Button from './Button';
+import ToggleButton from './ToggleButton';
+import CardItem from './CardItem';
+import ShareImg from '../shareImg/ShareImg';
+import { iconsMap, tokenNameMap } from '../../utils/config';
 
+import refreshIcon from '../../assets/icons/refresh.svg';
 import happyLendie from '../../assets/img/share-img/cats/happy.png';
 import standingLending from '../../assets/img/share-img/cats/standing.svg';
 import swagLendie from '../../assets/img/share-img/cats/swag.svg';
 import winkLendie from '../../assets/img/share-img/cats/wink.svg';
-import logo from '../../assets/icons/logo.svg'
-
-import dollarcatImage from '../../assets/img/share-img/dollar-cat.svg'
-import glasscatImage from '../../assets/img/share-img/glass-cat.svg'
-import rabbitcatImage from '../../assets/img/share-img/rabbit-cat.svg'
-import bottlecatImage from '../../assets/img/share-img/bottle-cat.svg'
-import ToggleButton from './ToggleButton';
-import CardItem from './CardItem';
-import Button from './Button';
-
-import ShareImg from '../shareImg/ShareImg';
-import { iconsMap, tokenNameMap } from '../../utils/config';
+import logo from '../../assets/icons/logo.svg';
+import dollarcatImage from '../../assets/img/share-img/dollar-cat.svg';
+import glasscatImage from '../../assets/img/share-img/glass-cat.svg';
+import rabbitcatImage from '../../assets/img/share-img/rabbit-cat.svg';
+import bottlecatImage from '../../assets/img/share-img/bottle-cat.svg';
 
 interface IShareImageModalProps {
   token: string;
@@ -32,45 +31,33 @@ const getDefaultImage = (): string => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-function ShareImageModal(
-  {
-    token,
-    apy,
-    dailyEarnings,
-    onClose,
-  }: IShareImageModalProps
-) {
-  const handleClickOutside = (
-    event: React.MouseEvent<HTMLDivElement>
-  ) => {
-    if (event.target === event.currentTarget) {
-      onClose();
-    }
-  };
-
-
-  console.log(token, apy, dailyEarnings)
-  const defaultCircleImage = getDefaultImage();
+function ShareImageModal({
+  token,
+  apy,
+  dailyEarnings,
+  onClose,
+}: IShareImageModalProps) {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null); // Track selected image index
   const [name, setName] = useState<string>('HyperLend user');
   const [fetchTwitterPhoto, setFetchTwitterPhoto] = useState<boolean>(false);
   const [errorMsg, setErrorMsg] = useState<string>('');
   const [rotationAngle, setRotationAngle] = useState(0);
-  const [circularImage, setCircularImage] =
-    useState<string>(defaultCircleImage);
+  const [circularImage, setCircularImage] = useState<string>(getDefaultImage());
+  const imageRefs = useRef<Array<HTMLDivElement | null>>([]); // Create array of refs
 
-  const handleNameChange = (e: any) => {
-    setName(e.target.value);
-  };
+  const handleNameChange = (e: any) => setName(e.target.value);
 
-  const handleRotate = () => {
-    setRotationAngle((prevAngle) => prevAngle + 360);
+  const handleRotate = () => setRotationAngle((prevAngle) => prevAngle + 360);
+
+  const handleClickOutside = (
+    event: React.MouseEvent<HTMLDivElement>
+  ) => {
+    if (event.target === event.currentTarget) onClose();
   };
 
   useEffect(() => {
     if (errorMsg) {
-      setTimeout(() => {
-        setErrorMsg('');
-      }, 4000);
+      setTimeout(() => setErrorMsg(''), 4000);
     }
   }, [errorMsg]);
 
@@ -80,11 +67,25 @@ function ShareImageModal(
 
   const getTwitterImage = () => {
     if (fetchTwitterPhoto && name) {
-      const imageURL = `https://unavatar.io/twitter/${name}`;
-      setCircularImage(imageURL);
+      setCircularImage(`https://unavatar.io/twitter/${name}`);
     } else {
-      setCircularImage(defaultCircleImage);
+      setCircularImage(getDefaultImage());
     }
+  };
+
+  const downloadImage = () => {
+    if (selectedIndex === null || imageRefs.current[selectedIndex] === null) return;
+
+    toPng(imageRefs.current[selectedIndex]!, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'image.png';
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.error('Could not download image', err);
+      });
   };
 
   return (
@@ -94,104 +95,79 @@ function ShareImageModal(
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         onClick={handleClickOutside}
-        className='fixed flex justify-center items-center top-0 left-0 w-full h-screen backdrop-blur-sm'
+        className="fixed flex justify-center items-center top-0 left-0 w-full h-screen backdrop-blur-sm"
       >
         <motion.div
           initial={{ scale: 0, rotate: '12.5deg' }}
           animate={{ scale: 1, rotate: '0deg' }}
           exit={{ scale: 0, rotate: '0deg' }}
-          className='shadow-4xl rounded-2xl'
+          className="shadow-4xl rounded-2xl"
         >
-          <CardItem className='px-5 sm:px-10 py-8 max-h-[calc(100vh-50px)] overflow-auto'>
-            <div className='flex flex-col justify-center items-center gap-5 text-center'>
-              <div className='flex flex-col gap-3 justify-center items-center'>
-                <div className=''>
-                  <img className=''
-                    src={logo} alt="logo" />
-                </div>
-                <p className='font-nexa font-black text-xl text-secondary'>
-                  Share your position
-                </p>
+          <CardItem className="px-5 sm:px-10 py-8 max-h-[calc(100vh-50px)] overflow-auto">
+            <div className="flex flex-col justify-center items-center gap-5 text-center">
+              <div className="flex flex-col gap-3 justify-center items-center">
+                <img src={logo} alt="logo" className="" />
+                <p className="font-nexa font-black text-xl text-secondary">Share your position</p>
               </div>
-              <div className='flex flex-col justify-center items-center gap-2'>
-                <p className='font-lufga text-sm font-light text-secondary'>
-                  X (Twitter) username{' '}
-                </p>
+              <div className="flex flex-col justify-center items-center gap-2">
+                <p className="font-lufga text-sm font-light text-secondary">X (Twitter) username{' '}</p>
                 <input
-                  autoFocus={true}
-                  type='text'
-                  className='form-control-plaintext text-center font-lufga text-xl text-secondary border-0 p-0 border-b-2 border-[#212325]'
-                  value={name == 'HyperLend user' ? '' : name}
-                  onChange={(e) => {
-                    handleNameChange(e);
-                  }}
-                  style={{
-                    background: 'transparent',
-                    outline: 'none',
-                    boxShadow: 'none',
-                    width: 'auto',
-                    minWidth: '50px',
-                  }}
+                  autoFocus
+                  type="text"
+                  className="form-control-plaintext text-center font-lufga text-xl text-secondary border-0 p-0 border-b-2 border-[#212325]"
+                  value={name === 'HyperLend user' ? '' : name}
+                  onChange={handleNameChange}
+                  style={{ background: 'transparent', outline: 'none', boxShadow: 'none', width: 'auto', minWidth: '50px' }}
                 />
               </div>
-              <div className='flex justify-center items-center gap-4 text-white text-sm'>
-                <p className=''>
-                  Use X (Twitter) profile photo?
-                </p>
-
-                <ToggleButton
-                  status={fetchTwitterPhoto}
-                  setStatus={setFetchTwitterPhoto} />
+              <div className="flex justify-center items-center gap-4 text-white text-sm">
+                <p>Use X (Twitter) profile photo?</p>
+                <ToggleButton status={fetchTwitterPhoto} setStatus={setFetchTwitterPhoto} />
                 <motion.img
                   src={refreshIcon}
-                  alt='refresh'
+                  alt="refresh"
                   style={{ cursor: 'pointer' }}
                   onClick={handleRotate}
-                  animate={{
-                    rotate: rotationAngle,
-                  }}
-                  transition={{
-                    duration: 1,
-                    ease: 'easeInOut',
-                  }}
+                  animate={{ rotate: rotationAngle }}
+                  transition={{ duration: 1, ease: 'easeInOut' }}
                 />
               </div>
 
-              {errorMsg ? (
-                <div className='flex justify-between mb-2'>
-                  <p className='font-lufga font-light text-xs text-[#FF0000] mt-2'>
-                    {errorMsg}
-                  </p>
+              {errorMsg && (
+                <div className="flex justify-between mb-2">
+                  <p className="font-lufga font-light text-xs text-[#FF0000] mt-2">{errorMsg}</p>
                 </div>
-              ) : (
-                ''
               )}
 
-              <div className='md:px-6 py-4 bg-[#050F0D] rounded-2xl flex flex-col gap-4'>
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-2 overflow-y-auto'>
-                  {
-                    shareItems.map((item) => (
+              <div className="md:px-6 py-4 bg-[#050F0D] rounded-2xl flex flex-col gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-2 overflow-y-auto">
+                  {shareItems.map((item, index) => (
+                    <div
+                      key={item.catImage}
+                      ref={(el) => (imageRefs.current[index] = el)} // Assign each image div a ref
+                      onClick={() => setSelectedIndex(index)} // Set selected index
+                      style={{
+                        transform: selectedIndex === index ? 'scale(0.95)' : 'scale(1)',
+                        cursor: 'pointer',
+                      }}
+                    >
                       <ShareImg
                         catImage={item.catImage}
                         circularImage={circularImage}
                         username={name}
                         symbol={tokenNameMap[token]}
                         apy={apy}
-                        dailyEarnings={dailyEarnings == '0' ? '0.000' : dailyEarnings}
+                        dailyEarnings={dailyEarnings === '0' ? '0.000' : dailyEarnings}
                         tokenIcon={iconsMap[tokenNameMap[token]]}
                       />
-                    ))
-                  }
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
             <div className="flex flex-col md:flex-row gap-4">
-              <Button
-                title='Share'
-                onClick={() => { }} />
-              <Button
-                title='Download Image'
-                onClick={() => { }} />
+              <Button title="Share" onClick={() => { }} />
+              <Button title="Download Image" onClick={downloadImage} />
             </div>
           </CardItem>
         </motion.div>
@@ -199,7 +175,6 @@ function ShareImageModal(
     </AnimatePresence>
   );
 }
-
 
 export default ShareImageModal;
 
--- src/components/shareImg/ShareImg.tsx
+++ src/components/shareImg/ShareImg.tsx
@@ -24,47 +24,45 @@ function ShareImg(
 ) {
 
   return (
-    <div>
-      <div className='pl-[20px] flex bg-[#CAEAE5] w-[100%] h-auto rounded-md'>
-        <div className='my-[25px] flex flex-col w-[30%] z-10'>
-          <div className='w-[160%] sm:w-[150%]'>
-            <div className='flex items-center justify-start w-full gap-1'>
-              <div className="relative">
-                <img src={circleImage} alt='circle' className='w-[28px] aspect-square' />
-                <img src={circularImage} alt='circle' className='w-[16px] rounded-full aspect-square absolute top-[6px] left-[6px]' />
-              </div>
-              <div className='font-lufga text-[7px] font-[300]'>
-                Meet {username}
-              </div>
+    <div className='pl-[20px] flex bg-[#CAEAE5] w-[100%] h-auto rounded-md'>
+      <div className='my-[25px] flex flex-col w-[30%] z-10'>
+        <div className='w-[160%] sm:w-[150%]'>
+          <div className='flex items-center justify-start w-full gap-1'>
+            <div className="relative">
+              <img src={circleImage} alt='circle' className='w-[28px] aspect-square' />
+              <img src={circularImage} alt='circle' className='w-[16px] rounded-full aspect-square absolute top-[6px] left-[6px]' />
             </div>
-            <div className='flex items-center mt-3 gap-1'>
-              <div className='flex items-center gap-1'>
-                <img src={tokenIcon} className='w-[14px]' alt='ether' />
-                <p className='font-lufga text-[8px]'>{symbol}</p>
-              </div>
-              <div className='font-lufga text-[8px] bg-black font-[500] text-[#CAEAE5] px-1 rounded-sm'>${dailyEarnings}/Day</div>
-            </div>
-
-            <div className='text-[36px] sm:text-[40px] font-nexa font-[900] text-left'>
-              {apy}%
-            </div>
-
-            <div className='font-lufga text-[5px] font-[400] text-left'>
-              Unlock powerful yields trough our secure lending protocol.
-            </div>
-
-            <div className='font-lufga text-[8px] text-left font-[800]'>
-              hyperlend.finance
-            </div>
-
-            <div className='flex items-center mt-5'>
-              <img src={logo} alt='logo' className='w-fit' />
+            <div className='font-lufga text-[7px] font-[300]'>
+              Meet {username}
             </div>
           </div>
+          <div className='flex items-center mt-3 gap-1'>
+            <div className='flex items-center gap-1'>
+              <img src={tokenIcon} className='w-[14px]' alt='ether' />
+              <p className='font-lufga text-[8px]'>{symbol}</p>
+            </div>
+            <div className='font-lufga text-[8px] bg-black font-[500] text-[#CAEAE5] px-1 rounded-sm'>${dailyEarnings}/Day</div>
+          </div>
+
+          <div className='text-[36px] sm:text-[40px] font-nexa font-[900] text-left'>
+            {apy}%
+          </div>
+
+          <div className='font-lufga text-[5px] font-[400] text-left'>
+            Unlock powerful yields trough our secure lending protocol.
+          </div>
+
+          <div className='font-lufga text-[8px] text-left font-[800]'>
+            hyperlend.finance
+          </div>
+
+          <div className='flex items-center mt-5'>
+            <img src={logo} alt='logo' className='w-fit' />
+          </div>
         </div>
-        <div className='w-[70%] flex justify-end overflow-hidden rounded-md'>
-          <img src={catImage} className="max-h-[219px]" alt='cat' />
-        </div>
+      </div>
+      <div className='w-[70%] flex justify-end overflow-hidden rounded-md'>
+        <img src={catImage} className="max-h-[219px]" alt='cat' />
       </div>
     </div>
   );
