5885e34a281ac3708c77ed1611125829a6083267
fbslo <admin@fbslo.net>
2024-11-01 23:18:00 +0100

    fix build & lint

--- src/utils/constants/constants.tsx
+++ src/utils/constants/constants.tsx
@@ -1,6 +1,6 @@
 import dashboardIcon from '../../assets/icons/dashboard-icon.svg';
 import lendborrowIcon from '../../assets/icons/lend-borrow-icon.svg';
-import analyticsIcon from '../../assets/icons/analytics-icon.svg';
+// import analyticsIcon from '../../assets/icons/analytics-icon.svg';
 // import hyperloopIcon from '../../assets/icons/hyperloop-icon.svg';
 
 type NavLinkProps = {
