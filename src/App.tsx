6029e0d249b2b94b2f08615f113017193faf48cf
konan-5 <ikedahiroshi517@gmail.com>
2024-11-01 20:37:32 +0100

    feat: Add toast for transaction view

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import Router from './router';
 import { AppKitProvider } from './provider/AppKitProvider';
 import ReactGA from 'react-ga4';
 import { ConfirmProvider } from './provider/ConfirmProvider';
+import { ToastProvider } from './provider/ToastProvider';
 
 function App() {
   ReactGA.initialize('G-Q79JWRWB61');
   return (
     <AppKitProvider>
       <ConfirmProvider>
-        <Router />
+        <ToastProvider>
+          <Router />
+        </ToastProvider>
       </ConfirmProvider>
     </AppKitProvider>
   );
