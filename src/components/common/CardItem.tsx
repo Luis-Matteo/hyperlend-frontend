ba491fbd41a69efc135f1f4dc5f64c205eeba09a
konan-5 <ikedahiroshi517@gmail.com>
2024-11-20 18:01:58 +0100

    feat: Update guide without using image

--- src/components/common/CardItem.tsx
+++ src/components/common/CardItem.tsx
@@ -1,14 +1,20 @@
-import { ReactNode } from 'react';
+import { forwardRef, ReactNode } from 'react';
 
+// Define the props for the CardItem component
 type CardItemProps = {
   className: string;
   children: ReactNode;
 };
 
-function CardItem({ className, children }: CardItemProps) {
-  return (
-    <div className={`rounded-2xl bg-primary ${className}`}>{children}</div>
-  );
-}
+// Forward ref to the div (or any element you want to apply it to)
+const CardItem = forwardRef<HTMLDivElement, CardItemProps>(
+  ({ className, children }, ref) => {
+    return (
+      <div ref={ref} className={`rounded-2xl bg-primary ${className}`}>
+        {children}
+      </div>
+    );
+  },
+);
 
 export default CardItem;
