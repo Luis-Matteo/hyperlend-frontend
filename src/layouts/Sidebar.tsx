c1a83f4c3b50ad21e51caa72fbf87cdd2a32dc5f
fbslo <admin@fbslo.net>
2024-12-16 03:25:57 +0100

    fix wrappedTokenProtocolTokens

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -132,6 +132,9 @@ function Sidebar() {
                     setFaucetButtonText('Faucet');
                     sendClaimTx();
                   }}
+                  onError={() => {
+                    sendClaimTx();
+                  }}
                 />
               ) : (
                 <button
