32745c7b21f2eb006980408deaff74e099ac97b4
fbslo <admin@fbslo.net>
2024-12-23 15:31:38 +0100

    remove vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "hyperlend",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
 
   build: {
     sourcemap: true
