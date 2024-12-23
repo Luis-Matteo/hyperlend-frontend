a7db577a81bf29908fbfba39f1915a5b7ed27470
fbslo <admin@fbslo.net>
2024-12-23 19:28:49 +0100

    fix getAvailableRepay

--- src/utils/user/core/functions/utils.tsx
+++ src/utils/user/core/functions/utils.tsx
@@ -98,7 +98,7 @@ function getAvailableWithdraw(params: any) {
   const userBalanceToken = supplied?.balance || 0;
   const userBalanceValueUsd = userBalanceToken * tokenPriceUsd;
 
-  if (userBalanceToken == 0) return 0;
+  if (!userBalanceToken || userBalanceToken == 0) return 0;
 
   const maxWithdrawableUsd =
     (Number(totalCollateralBase) -
@@ -152,7 +152,7 @@ function getAvailableRepay(params: any) {
     params.userPositionsData.borrowed.find(
       (e: any) => e.underlyingAsset == params.token,
     )?.balance || 0;
-  if (borrowedBalance == 0) return 0;
+  if (!borrowedBalance || borrowedBalance == 0) return 0;
 
   const userBalance = normalizeDecimalsAmount(
     wrappedTokens.includes(params.token)
