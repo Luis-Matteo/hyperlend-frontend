291653980274323d8a5882245acf217a46bd4272
fbslo <admin@fbslo.net>
2024-12-27 18:18:58 +0100

    remove unused code

--- src/utils/protocol/faucet.tsx
+++ src/utils/protocol/faucet.tsx
@@ -1,23 +1,4 @@
-// import { getBlock } from '@wagmi/core';
-// import { config } from '../../provider/AppKitProvider';
-
 export async function claimFaucet(token: any, userAddress?: string) {
-  // const blockData = await getBlock(config);
-  // let { hash, number } = blockData;
-
-  // const powChallenge = `${hash}-${number}-${userAddress}`;
-  // const difficulty = 1;
-
-  // let powResult: string = '';
-  // let nonce: number = 0;
-  // while (
-  //   powResult.substring(0, difficulty) !== Array(difficulty + 1).join('0')
-  // ) {
-  //   nonce++;
-  //   powResult = (await sha256(powChallenge + nonce)).toString();
-  // }
-
-  // const challengeResult = `${userAddress}:${number}:${nonce}`;
   const challengeV2Text =
     'If you are running the farming bot, stop wasting your time. Testnet will not be directly incentivized, and mainnet airdrop will be linear with a minimum threshold.';
   const requestOptions = {
@@ -55,13 +36,3 @@ export async function claimFaucet(token: any, userAddress?: string) {
       });
   });
 }
-
-// async function sha256(message: any) {
-//   const msgBuffer = new TextEncoder().encode(message);
-//   const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-//   const hashArray = Array.from(new Uint8Array(hashBuffer));
-//   const hashHex = hashArray
-//     .map((b) => b.toString(16).padStart(2, '0'))
-//     .join('');
-//   return hashHex;
-// }
